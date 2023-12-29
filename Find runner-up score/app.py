7e8c85f47c4cb157220dcabf9a6c6f7f7659c328
Qurbani <64720446+qurbaniemrahov@users.noreply.github.com>
2023-12-29 10:59:34 +0000

    solved

--- Find runner-up score/app.py
+++ Find runner-up score/app.py
@@ -1,17 +1,18 @@
-def findRunner():
-    arr = [2, 3, 6, 6, 5]
-    maximum = max(arr)
-    second = []
+if __name__ == '__main__':
+    n = int(input())  
+    arr = list(map(int, input().split()))  
 
-    for n in arr:
-        if n < maximum:
-            second.append(n)
+    def runner(arr):
+        maximum = max(arr)
+        second = []
+        for i in arr:
+            if i < maximum:
+                second.append(i)
+        print(max(second))
 
-   
-    second_largest = max(second)
-    print(second_largest)
+    runner(arr) 
+        
 
-findRunner()
 
 
     
